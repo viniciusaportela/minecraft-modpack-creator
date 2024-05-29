1d19b0faacfa5042758b4096db0a4fa2f9f3b55a
viniciusaportela <vinicius.portela.stm@gmail.com>
2024-05-29 19:20:21 -0300

    fix: broken build on windows

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,5 @@
 import './App.css';
-import {
-  lazy,
-  LazyExoticComponent,
-  Suspense,
-  useLayoutEffect,
-  useState,
-} from 'react';
+import { Suspense, useLayoutEffect, useState } from 'react';
 import { NextUIProvider, Progress } from '@nextui-org/react';
 import { ipcRenderer } from 'electron';
 import Realm from 'realm';
@@ -17,23 +11,10 @@ import { BlockModel } from './core/models/block.model';
 import { ModModel } from './core/models/mod.model';
 import { GlobalStateModel } from './core/models/global-state.model';
 import { useAppStore } from './store/app.store';
+import MainApp from './MainApp';
+import Picker from './pages/picker/Picker';
 
 import { ModConfigModel } from './core/models/mod-config.model';
-
-let MainApp:
-  | typeof import('./MainApp')
-  | LazyExoticComponent<typeof import('./MainApp').default>;
-let Picker:
-  | typeof import('./pages/picker/Picker')
-  | LazyExoticComponent<typeof import('./pages/picker/Picker').default>;
-
-if (process.env.NODE_ENV === 'development') {
-  MainApp = require('./MainApp').default;
-  Picker = require('./pages/picker/Picker').default;
-} else {
-  MainApp = lazy(() => import('./MainApp'));
-  Picker = lazy(() => import('./pages/picker/Picker'));
-}
 
 export default function App() {
   const urlParams = new URLSearchParams(window.location.search);
